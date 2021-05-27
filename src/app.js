0a0915af6ba16814a817120bf2d188d7d424dedf
Phimanshu1234 <pandey101299@gmail.com>
2021-05-28 00:01:08 +0530

    Weather-application

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('',(req,res)=>{
     res.render('index',{
         title: 'Weather App',
-        name: 'Riyansh Pal'
+        name: 'Himanshu Pandey'
     })
 })
 
 app.get('/about',(req,res)=>{
     res.render('about',{
         title: 'About',
-        name: 'Riyansh Pal'
+        name: 'Himanshu Pandey'
     })
 })
 
